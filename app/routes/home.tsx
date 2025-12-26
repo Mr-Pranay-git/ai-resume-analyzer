7a0c9038ae334691fc45e8a3c9f710999fab99c5
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-12-26 14:31:45 +0530

    basic title and heading is Created

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
+import Navebar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "~/constants";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyzer" },
+    { name: "description", content: "Get actual feedback of our Resume" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  function callbackfn(value: Resume, index: number, array: Resume[]): ReactNode {
+    throw new Error("Function not implemented.");
+  }
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navebar/>
+
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissons and check AI- powered feedback</h2>
+      </div>
+    </section>
+
+    {/* {resumes.map(callbackfn: (resume)=>(
+      <div>
+        <h1>{resume.jobTitle}</h1>
+      </div>
+    ))} */}
+  </main>;
 }
