a3eba8bb38eb9d496f7243b766be49f8aec9af1d
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-01-01 19:55:43 +0530

    added nullish coalescing operator in ResumeCard component

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -6,8 +6,8 @@ const ResumeCard = ({resume:{id,companyName,jobTitle, feedback, imagePath}}:{res
     <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
       <div className="resume-card-header">
         <div className='flex flex-col gap-2 '>
-        <h2 className=' text-black! font-bold wrap-break-word'>{companyName}</h2>
-        <h3 className="text-lg wrap-break-word text-gray-500">{jobTitle}</h3>
+        <h2 className=' text-black! font-bold wrap-break-word'>{companyName ?? "Unknown Company"}</h2>
+        <h3 className="text-lg wrap-break-word text-gray-500">{jobTitle ?? "Unknown Role"}</h3>
       </div>
       <div className="shrink-0">
           <ScoreCircle score={feedback.overallScore}/> 
