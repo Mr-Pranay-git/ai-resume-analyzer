58dca266dbe60799482c96d73055737f797c5445
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-01-14 17:51:32 +0530

    more efficient auth is added

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -8,13 +8,25 @@
     ])
 
     const Auth = () => {
+        // -------------More Effieicent------------------------------------
         const {isLoading, auth} = usePuterStore();
         const location = useLocation();
-        const next = location.search.split('next=')[1];
+        const next = new URLSearchParams(location.search).get("next") || '/'
         const navigate = useNavigate();
         useEffect(()=>{
-            if(auth.isAuthenticated) navigate(next);
+            if(auth.isAuthenticated) {
+                navigate(next,{replace: true});
+            }
         },[auth.isAuthenticated, next])
+
+        //----------------------OR(tutorials auth system)-----------------------------
+        // const {isLoading, auth} = usePuterStore();
+        // const location = useLocation();
+        // const next = location.search.split('next=')[1];
+        // const navigate = useNavigate();
+        // useEffect(()=>{
+        //     if(auth.isAuthenticated) navigate(next);
+        // },[auth.isAuthenticated, next])
             
             return (
                 <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
