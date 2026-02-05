50b95f8d5a05fbca52654e4bbd9c6244d1410dd4
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-02-05 23:59:55 +0530

    created resume page for making UI

---
+++ app/routes/resume.tsx
@@ -0,0 +1,12 @@
+import { useParams } from 'react-router'
+
+const Resume = () => {
+    const {id}= useParams();
+  return (
+    <div>
+      Resume {id}
+    </div>
+  )
+}
+
+export default Resume
