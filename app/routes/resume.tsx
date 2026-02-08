cb23c040e334b4065cd1375a045b874753ad589f
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-02-08 23:55:00 +0530

    work in progress

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -15,6 +15,12 @@ const Resume = () => {
           <span className="text-gray-800 text-sm font-semibold">Back to Homepage</span>
         </Link>
       </nav>
+      <div className='flex flex-row w-full max-lg:flex-col-reverse'>
+        <section className='feedback-section animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit'>
+          
+        </section>
+        
+      </div>
     </main>
   )
 }
