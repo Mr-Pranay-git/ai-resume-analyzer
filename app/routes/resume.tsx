b81ee0ad89e25b7e4849a61140efd9a0eab7f8d2
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-02-13 23:59:05 +0530

    resume review

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -52,7 +52,7 @@ const Resume = () => {
         <section className='feedback-section bg-[url("/images/bg-small.svg") bg-cover h-screen sticky top-0 items-center justify-center] '>
           {imageUrl && resumeUrl && (
             <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
-              <a>
+              <a href={resumeUrl} target="_blank" rel='noopener noreferrer'>
                 <img 
                   src={imageUrl}
                   className="w-full h-full object-contain rounded-2xl"
