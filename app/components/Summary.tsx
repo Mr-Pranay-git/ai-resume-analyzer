9b31f1541f0a99d2d526f5dd468a02345b75f021
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-02-16 00:48:06 +0530

    score added

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,8 +1,19 @@
 import ScoreGauge from './ScoreGauge';
 const Category = ({title, score}: {title:string, score: number}) =>{
+  const textColor = score>70 ? 'text-green-600'
+    : score>40 
+    ? 'text-yellow-600' : 'text-red-600';
+
   return(
     <div className="resume-summary">
-      {title}
+      <div className="category">
+        <div className='flex flex-row gap-2 items-center justify-center'>
+          <p className="text-2xl">{title}</p>
+        </div>
+        <p className="text-2xl">
+          <span className={textColor}>{score}</span>
+        </p>
+      </div>
     </div>
   )
 }
@@ -20,6 +31,11 @@ const Summary = ({feedback}:{feedback: Feedback}) => {
           </p>
         </div>
       </div>            
+
+      <Category title="Tone & Style" score={feedback.toneAndStyle.score} />
+      <Category title="Content" score={feedback.content.score} />
+      <Category title="Structure" score={feedback.structure.score} />
+      <Category title="Skill" score={feedback.skills.score} />
     </div>
   )
 }
